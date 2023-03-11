b572bef818d7c1b87ed3ab9c47be00b5d2aefee4
Chikatanaka <ume12.vmd38@gmail.com>
2023-03-11 04:15:56 +0000

    PController変更

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -10,14 +10,21 @@ class SchoolController extends Controller
     {
     
         $word = $request->get('word')
-        $school = School:schools();
-        if(isset($word)){
+        $school = School:school();
+        if(isset($word){
             $array_words = preg_split('/\s;ui',$word,-1,PREG_SPLIT_NO_EMPTY);
         foreach($array_words){
             $escape_word = addcslashes($w,'\\_%');
-            $school = $school->where(DB::raw("CONCAT('school','','qualification','','address','','area')"),'like','%',$escape_word.'%');
+            $school = $school->where(DB::raw("CONCAT('school','','address')"),'like','%',$escape_word.'%');
         }
     }
+    if(is_array($request->input('prefs'))){
+
+    $query->where(function($q) use($request){
+        foreach($request->input('prefs') as $pref){
+            $q->orWhere('pref',$pref);
+        }
+    });
     $articles = $school->get();
         return view('front',['posts' =>$posts]);
     }
